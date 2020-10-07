c56eaa72b066d22f25c33c1973e73f54644b9118
Jonathon Joyner <jonathonandon@gmail.com>
2020-10-06 20:49:00 -0500

    setup boards to show on dom

---
+++ src/javascripts/components/cards/Boards.js
@@ -0,0 +1,41 @@
+import deleteBoard from '../../helpers/data/boardData';
+
+const boardMaker = (board) => {
+  const domString = `<div class="card card-cascade narrower" id="${board.uid}">
+
+  <div class="view view-cascade overlay">
+    <img class="card-img-top" src="${board.image}"
+      alt="Card image cap">
+    <a>
+      <div class="mask rgba-white-slight"></div>
+    </a>
+  </div>
+
+  <!-- Card content -->
+  <div class="card-body card-body-cascade">
+
+    <!-- Label -->
+    <h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i> Culinary</h5>
+    <!-- Title -->
+    <h4 class="font-weight-bold card-title">${board.name}</h4>
+    <!-- Text -->
+    <p class="card-text">Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
+      laboriosam, nisi ut aliquid ex ea commodi.</p>
+    <!-- Button -->
+     <div class="board-body">
+    <button id="${board.uid}" class="btn btn-dark delete-board board-buttons"><i class="far fa-trash-alt"></i> Delete Board </button>
+    </div>
+  </div>
+
+</div>`;
+  $('body').on('click', '.delete-board', (e) => {
+    e.stopImmediatePropagation();
+    const firebaseKey = e.currentTarget.id;
+    $(`.card#${firebaseKey}`).remove();
+    deleteBoard.deleteBoard(firebaseKey);
+  });
+
+  return domString;
+};
+
+export default { boardMaker };
--- src/javascripts/components/myNavbar/myNavbar.js
+++ src/javascripts/components/myNavbar/myNavbar.js
@@ -16,21 +16,21 @@ const logoutEvent = () => {
 const myNavbar = (user) => {
   $('#nav').html(
     `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Horsey Cow Pasture</a>
+      <a class="navbar-brand" href="#">Pinterest 2.0</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
       aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-      <li class="nav-item mx-3"  id="farmers-link">
-        <a class="nav-link" href="#"><i class="fas fa-hat-cowboy-side"></i>Boards</a>
+      <li class="nav-item mx-3"  id="all-boards-link">
+        <a class="nav-link" href="#"><i class="fas fa-clipboard"></i>Boards</a>
       </li>
-      <li class="nav-item mx-3" id="cows-link">
-        <a class="nav-link" href="#"><i class="far fa-cow"></i><i class="fas fa-horse"></i> Cows</a>
+      <li class="nav-item mx-3" id="add-boards-link">
+        <a class="nav-link" href="#"><i class="fas fa-clipboard-check"></i>Add a board</a>
       </li>
-      <li class="nav-item mx-3" id="add-cow-link">
-        <a class="nav-link" href="#"><i class="fas fa-plus-circle"></i> Add A Cow</a>
+      <li class="nav-item mx-3" id="add-pin-link">
+        <a class="nav-link" href="#"><i class="fas fa-plus-circle"></i> Add A Pin</a>
       </li>
     </ul>
         <ul class="navbar-nav ml-auto">
