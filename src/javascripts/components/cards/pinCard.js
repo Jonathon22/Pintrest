eb0690d48722fd4878a95b9849448c1ea39d4d21
Jonathon Joyner <jonathonandon@gmail.com>
2020-10-09 21:25:10 -0500

    added erd to readme

--- src/javascripts/components/cards/pinCard.js
+++ src/javascripts/components/cards/pinCard.js
@@ -5,8 +5,8 @@ const pinMaker = (pinObject) => {
   <a href=“” target=“_blank”><img src=“${pinObject.image}” class=“card-img-top” alt=““></a>
   <div class=“card-body”>
     <h5 class=“card-title”>${pinObject.name}</h5>
-    <a href=“#” id=“${pinObject.firebaseKey}” class=“btn btn-primary update-pin"><i class=“far fa-edit”></i>Update Pin</a>
-    <a href=“#” id=“${pinObject.firebaseKey}” class=“btn btn-danger delete-pin”>Delete</a>
+    <a href=“#” id=“” class=“btn btn-primary update-pin"><i class=“far fa-edit”></i>Update Pin</a>
+    <a href=“#” id=“” class=“btn btn-danger delete-pin”>Delete</a>
   </div>
 </div>`;
   $('body').on('click', '.delete-board', (e) => {
